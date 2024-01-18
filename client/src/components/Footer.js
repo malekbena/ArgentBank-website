8e16a218aa698dddf3262941a7125b46922c6efe
malekbena <malekb.benali@gmail.com>
2024-01-18 15:05:19 +0100

    footer integration

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <footer class="footer">
+            <p class="footer-text">Copyright 2020 Argent Bank</p>
+        </footer>
+    )
+}
+
+export default Navbar
