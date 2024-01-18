87adea654d3778b0b42f081845443d169bd35261
malekbena <malekb.benali@gmail.com>
2024-01-18 14:46:58 +0100

    navbar component

--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
     return (
-        <div>
-            <p>Layout</p>
+        <>
+            <Navbar />
             <Outlet />
-        </div>
+        </>
     );
 }
 export default Layout
