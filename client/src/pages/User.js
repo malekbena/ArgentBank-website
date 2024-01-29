539f79fbadfadef623fdf84e977f5bf1f080f6bf
malekbena <malekb.benali@gmail.com>
2024-01-29 16:31:41 +0100

    AccountItem component on user page

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
-import Button from "../components/Button"
 import { userEdit } from "../redux/userSlice"
+import Button from "../components/Button"
+import AccountItem from "../components/AccountItem"
 
 const User = () => {
   const [editForm, setEditForm] = useState(false)
@@ -68,36 +69,9 @@ const User = () => {
         }
       </div>
       <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className={'classic-button transaction-button'} text='View transactions' />
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className={'classic-button transaction-button'} text='View transactions' />
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <Button className={'classic-button transaction-button'} text='View transactions' />
-        </div>
-      </section>
+      <AccountItem title={'Argent Bank Checking (x8349)'} amount={'$2,082.79'} description={'Available Balance'} />
+      <AccountItem title={'Argent Bank Savings (x6712)'} amount={'$10,928.42'} description={'Available Balance'} />
+      <AccountItem title={'Argent Bank Credit Card (x8349)'} amount={'$184.30'} description={'Current Balance'} />
     </main>
   )
 }
